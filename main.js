a20bf6d6d7a7d33a4a168651bcec480060141dca
Mohamed-apdi <119160455+Mohamed-apdi@users.noreply.github.com>
2023-06-03 17:45:09 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url =
+    "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            jokeContainer.textContent = `${item.joke}`;
+            jokeContainer.classList.add("fade");
+            localStorage.setItem("item", item);
+        });
+};
+btn.addEventListener("click", getJoke);
+getJoke();
+if (localStorage.getItem("item").length > 0) {
+    getJoke(localStorage.getItem("item"));
+}
